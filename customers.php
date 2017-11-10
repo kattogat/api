7504ebe177fe30e0a31216c6ca4f61cacc1bf203
NOVA\maria <maria_hav@live.se>
2017-11-10 09:53:57 +0100

    Fixed disapering id

--- customers.php
+++ customers.php
@@ -10,7 +10,8 @@ header('Content-type: application/json');
 // $array = $stmt->fetchAll();
 // $json = json_encode($array, JSON_PRETTY_PRINT);
 
-$sql = "SELECT * FROM `user` INNER JOIN `address` ON address.customer_id = user.existing_id";
+$sql = "SELECT user.*, address.existing_id AS address_id, address.customer_id, address.customer_id, address.customer_address_id, address.email, address.firstname, address.lastname, address.postcode, address.street, 
+address.city, address.telephone, address.country_id, address.address_type, address.company, address.country, address.existing_id FROM `user` LEFT JOIN `address` ON address.customer_id = user.existing_id";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
 $array = $stmt->fetchAll();
