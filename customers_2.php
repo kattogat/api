3d0e793ce20a58ad6fa6e8a470069160a85b6b50
NOVA\maria <maria_hav@live.se>
2017-11-10 10:57:34 +0100

    Exercise 3

--- customers_2.php
+++ customers_2.php
@@ -8,14 +8,20 @@
 
 require "config.php";
 
-$thisUser = $_GET['customer_id'];
+if (isset($_GET['customer_id'])) {
+    $thisUser = $_GET['customer_id'];
 
-$sql = "SELECT user.*, address.existing_id AS address_id, address.customer_id, address.customer_id, address.customer_address_id, address.email AS address_email, address.firstname AS address_firstname, address.lastname AS address_lastname, address.postcode, address.street, 
-address.city, address.telephone, address.country_id, address.address_type, address.company, address.country, address.existing_id FROM `user` LEFT JOIN `address` ON address.customer_id = user.existing_id WHERE user.existing_id = $thisUser";
-$stmt = $pdo->prepare($sql);
-$stmt->execute();
-$array = $stmt->fetchAll();
-$json = json_encode($array, JSON_PRETTY_PRINT);
+    $sql = "SELECT user.*, address.existing_id AS address_id, address.customer_id, address.customer_id, address.customer_address_id, address.email AS address_email, address.firstname AS address_firstname, address.lastname AS address_lastname, address.postcode, address.street, 
+    address.city, address.telephone, address.country_id, address.address_type, address.company, address.country, address.existing_id FROM `user` LEFT JOIN `address` ON address.customer_id = user.existing_id WHERE user.existing_id = $thisUser";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $array = $stmt->fetchAll();
+    $json = json_encode($array, JSON_PRETTY_PRINT);
 
-
-echo $json;
+    if ($json == "[]") {
+        header("HTTP/1.0 404 Not found");
+        echo "<h1>Error 404! Den här användaren finns inte!</h1>";
+    } else {
+        echo $json;
+    }
+}
