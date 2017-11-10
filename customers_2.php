5f5399ac1737144b206dded40bc51d1e31a0170d
NOVA\maria <maria_hav@live.se>
2017-11-10 11:46:54 +0100

    Exercise 4 (with bonus)

--- customers_2.php
+++ customers_2.php
@@ -1,6 +1,8 @@
 <form method="get" action="customers_2.php">
     <label>Sök på id</label><br>
     <input name="customer_id" type="number"><br>  
+    <label>Bara visa adress?</label><br>
+    <input name="address" type="checkbox" value="true"><br>  
     <input type="submit" text="Sänd">  
 </form>
 
@@ -8,7 +10,16 @@
 
 require "config.php";
 
-if (isset($_GET['customer_id'])) {
+function printOut($json) {
+    if ($json == "[]") {
+        header("HTTP/1.0 404 Not found");
+        echo "<h1>Error 404! Den här användaren finns inte!</h1>";
+    } else {
+        echo $json;
+    }
+}
+
+if (isset($_GET['customer_id']) and isset($_GET['address']) == false) {
     $thisUser = $_GET['customer_id'];
 
     $sql = "SELECT user.*, address.existing_id AS address_id, address.customer_id, address.customer_id, address.customer_address_id, address.email AS address_email, address.firstname AS address_firstname, address.lastname AS address_lastname, address.postcode, address.street, 
@@ -18,10 +29,16 @@ if (isset($_GET['customer_id'])) {
     $array = $stmt->fetchAll();
     $json = json_encode($array, JSON_PRETTY_PRINT);
 
-    if ($json == "[]") {
-        header("HTTP/1.0 404 Not found");
-        echo "<h1>Error 404! Den här användaren finns inte!</h1>";
-    } else {
-        echo $json;
-    }
+    printOut($json);
+
+} elseif (isset($_GET['address'])) {
+    $thisUser = $_GET['customer_id'];
+    
+        $sql = "SELECT * FROM `address` WHERE customer_id = $thisUser";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $array = $stmt->fetchAll();
+        $json = json_encode($array, JSON_PRETTY_PRINT);
+    
+        printOut($json);
 }
