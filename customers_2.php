1cdc43bd0f05716f32f0ea0bdb9765de5917df60
NOVA\maria <maria_hav@live.se>
2017-11-10 10:44:40 +0100

    Exercise 2

---
+++ customers_2.php
@@ -0,0 +1,21 @@
+<form method="get" action="customers_2.php">
+    <label>Sök på id</label><br>
+    <input name="customer_id" type="number"><br>  
+    <input type="submit" text="Sänd">  
+</form>
+
+<?php
+
+require "config.php";
+
+$thisUser = $_GET['customer_id'];
+
+$sql = "SELECT user.*, address.existing_id AS address_id, address.customer_id, address.customer_id, address.customer_address_id, address.email AS address_email, address.firstname AS address_firstname, address.lastname AS address_lastname, address.postcode, address.street, 
+address.city, address.telephone, address.country_id, address.address_type, address.company, address.country, address.existing_id FROM `user` LEFT JOIN `address` ON address.customer_id = user.existing_id WHERE user.existing_id = $thisUser";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$array = $stmt->fetchAll();
+$json = json_encode($array, JSON_PRETTY_PRINT);
+
+
+echo $json;
