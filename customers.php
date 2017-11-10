24e5bebcdd14fe3c92dc03a1036c0eed7f8b4128
NOVA\maria <maria_hav@live.se>
2017-11-10 09:57:18 +0100

    And fix for other disapering stuff

--- customers.php
+++ customers.php
@@ -10,7 +10,7 @@ header('Content-type: application/json');
 // $array = $stmt->fetchAll();
 // $json = json_encode($array, JSON_PRETTY_PRINT);
 
-$sql = "SELECT user.*, address.existing_id AS address_id, address.customer_id, address.customer_id, address.customer_address_id, address.email, address.firstname, address.lastname, address.postcode, address.street, 
+$sql = "SELECT user.*, address.existing_id AS address_id, address.customer_id, address.customer_id, address.customer_address_id, address.email AS address_email, address.firstname AS address_firstname, address.lastname AS address_lastname, address.postcode, address.street, 
 address.city, address.telephone, address.country_id, address.address_type, address.company, address.country, address.existing_id FROM `user` LEFT JOIN `address` ON address.customer_id = user.existing_id";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
